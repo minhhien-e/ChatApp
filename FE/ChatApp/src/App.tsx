39f86c3f305914bf8006a610477c6b3c1e6f1b29
minhhien <minhhien6634@gmail.com>
2025-05-27 16:58:45 +0700

    Initial project

---
+++ FE/ChatApp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+
+export default App;
