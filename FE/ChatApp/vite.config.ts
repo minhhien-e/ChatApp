35caa3a75f24a9516373bb6087fa22d2573022cb
minhhien <minhhien6634@gmail.com>
2025-05-29 17:10:51 +0700

    feat: update dependencies and improve path aliases
    - Added new dependencies: @radix-ui/react-dialog, @radix-ui/react-label, @radix-ui/react-slot, date-fns, framer-motion, and @vitejs/plugin-react.
    - Removed unused react.svg asset.
    - Updated tsconfig.app.json to extend from tsconfig.json.
    - Enhanced path aliases in tsconfig.json for better module resolution.
    - Refactored vite.config.ts to use fileURLToPath for alias resolution.

--- FE/ChatApp/vite.config.ts
+++ FE/ChatApp/vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   resolve: {
+  resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
+      "@types": fileURLToPath(new URL("./src/types", import.meta.url)),
+      "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
+      "@lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+      "@context": fileURLToPath(new URL("./src/context", import.meta.url)),
+      "@services": fileURLToPath(new URL("./src/services", import.meta.url)),
     },
   },
 });
